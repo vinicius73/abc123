542bbf8385c4672593c77d9712c9e94fd502653f
Vinicius Reis <luiz.vinicius73@gmail.com>
2022-12-24 17:36:19 -0300

    ➕ add prettier and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-essential', 'xo', 'prettier'],
+  overrides: [
+    {
+      extends: ['xo-typescript', 'prettier'],
+      files: ['*.ts', '*.tsx'],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {},
+};
